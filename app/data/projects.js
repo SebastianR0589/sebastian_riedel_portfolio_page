227ad23f9560184f073e25339114c3c8c751b51a
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-12-30 16:49:36 +0100

    added new project fullstack react spring boot manga review

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,13 +1,19 @@
-import { SiReact, SiNextdotjs, SiNodedotjs, SiExpress, SiPostgresql, SiSpringboot } from "react-icons/si";
+import {
+  SiReact,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiExpress,
+  SiPostgresql,
+  SiSpringboot,
+  SiMongodb
+} from "react-icons/si";
 
 export const projects = {
   frontend: [
-        {
+    {
       name: "CRUD app sample",
       framework: "React",
-  tech: [
-      { icon: <SiReact size={32} />, color: "#61dafb", name: "React" },
-    ],
+      tech: [{ icon: <SiReact size={32} />, color: "#61dafb", name: "React" }],
       images: [
         "/projects/crud/standard.PNG",
         "/projects/crud/add.PNG",
@@ -19,9 +25,7 @@ export const projects = {
     {
       name: "Tenzies Game",
       framework: "React",
-tech: [
-      { icon: <SiReact size={32} />, color: "#61dafb", name: "React" },
-    ],
+      tech: [{ icon: <SiReact size={32} />, color: "#61dafb", name: "React" }],
       images: [
         "/projects/tenzies/default.PNG",
         "/projects/tenzies/mid_game.PNG",
@@ -30,27 +34,23 @@ tech: [
       link: "https://github.com/SebastianR0589/tenzies_game_project",
       vercel: "https://sebastian-riedel-tenzies-game-proje.vercel.app/",
     },
-     {
+    {
       name: "Hangman Game",
       framework: "React",
-tech: [
-      { icon: <SiReact size={32} />, color: "#61dafb", name: "React" },
-    ],
+      tech: [{ icon: <SiReact size={32} />, color: "#61dafb", name: "React" }],
       images: [
         "/projects/hangman/active_game.PNG",
-         "/projects/hangman/lost_game.PNG",
-          "/projects/hangman/new_game.PNG",
-            "/projects/hangman/won_game.PNG",
+        "/projects/hangman/lost_game.PNG",
+        "/projects/hangman/new_game.PNG",
+        "/projects/hangman/won_game.PNG",
       ],
       link: "https://github.com/SebastianR0589/hangman_game_project",
       vercel: "https://sebastian-riedel-hangman-game-proje.vercel.app/",
     },
-       {
+    {
       name: "Manga Recommendations App",
       framework: "React",
-tech: [
-      { icon: <SiReact size={32} />, color: "#61dafb", name: "React" },
-    ],
+      tech: [{ icon: <SiReact size={32} />, color: "#61dafb", name: "React" }],
       images: [
         "/projects/manga/main_filled_out.PNG",
         "/projects/manga/main_interface.PNG",
@@ -60,12 +60,28 @@ tech: [
       vercel: "https://sebastian-riedel-manga-recommendati.vercel.app/",
     },
   ],
-  fullstack: [  {
+  fullstack: [
+     {
+      name: "Fullstack React Java Spring Boot Manga Review Sample",
+      framework: "React + Springboot",
+      tech: [{ icon: <SiReact size={10} />, color: "#61dafb", name: "React" }, { icon: <SiSpringboot size={10} />, color: "#6DB33F", name: "Springboot" }, { icon: <SiMongodb size={10} />, color: "#00ED64", name: "MongoDB" }],
+      images: [
+        "/projects/fullstack_java_react_manga_review/frontend.PNG",
+        "/projects/fullstack_java_react_manga_review/mongodb.PNG",
+        "/projects/fullstack_java_react_manga_review/backend.PNG",
+      ],
+      link: "https://github.com/SebastianR0589/fullstack_react_springboot_manga_review_app",
+    },
+    {
       name: "Portfolio Page",
       framework: "Next.js",
       tech: [
-      { icon: <SiNextdotjs size={32} />, color: "#f1f1f1ff", name: "Next.js" },
-    ],
+        {
+          icon: <SiNextdotjs size={32} />,
+          color: "#f1f1f1ff",
+          name: "Next.js",
+        },
+      ],
       images: [
         "/projects/portfolio/hero_section.PNG",
         "/projects/portfolio/skills_section.PNG",
@@ -80,8 +96,12 @@ tech: [
       name: "Java Springboot API Sample Runs",
       framework: "Java Springboot",
       tech: [
-      { icon: <SiSpringboot size={32} />, color: "#6DB33F", name: "Springboot" },
-    ],
+        {
+          icon: <SiSpringboot size={32} />,
+          color: "#6DB33F",
+          name: "Springboot",
+        },
+      ],
       images: [
         "/projects/java_rest_api_sample/files.PNG",
         "/projects/java_rest_api_sample/controller.PNG",
@@ -92,9 +112,9 @@ tech: [
     {
       name: "Node.js REST API Sample Travel Locations",
       framework: "Node.js",
-          tech: [
-      { icon: <SiNodedotjs size={32} />, color: "#339933", name: "Node.js" },
-    ],
+      tech: [
+        { icon: <SiNodedotjs size={32} />, color: "#339933", name: "Node.js" },
+      ],
       images: [
         "/projects/node_rest_api_sample/data.PNG",
         "/projects/node_rest_api_sample/utils.PNG",
@@ -102,12 +122,12 @@ tech: [
       ],
       link: "https://github.com/SebastianR0589/REST_API_sample_travel_locations",
     },
-  {
+    {
       name: "Paranormal Sightings app",
       framework: "Node.js",
-       tech: [
-      { icon: <SiNodedotjs size={32} />, color: "#339933", name: "Node.js" },
-    ],
+      tech: [
+        { icon: <SiNodedotjs size={32} />, color: "#339933", name: "Node.js" },
+      ],
       images: [
         "/projects/paranormal_sightings/home.PNG",
         "/projects/paranormal_sightings/read.PNG",
@@ -115,12 +135,16 @@ tech: [
       ],
       link: "https://github.com/SebastianR0589/Paranormal_Sightings_App",
     },
-      {
+    {
       name: "Express.js API Sample startups",
       framework: "Express.js",
-             tech: [
-      { icon: <SiExpress size={32} />, color: "#f1f1f1ff", name: "Express.js" },
-    ],
+      tech: [
+        {
+          icon: <SiExpress size={32} />,
+          color: "#f1f1f1ff",
+          name: "Express.js",
+        },
+      ],
       images: [
         "/projects/express_api_sample/server.PNG",
         "/projects/express_api_sample/controller.PNG",
@@ -131,9 +155,13 @@ tech: [
     {
       name: "Express.js Vinyl Store",
       framework: "Express.js",
-         tech: [
-      { icon: <SiExpress size={32} />, color: "#f1f1f1ff", name: "Express.js" },
-    ],
+      tech: [
+        {
+          icon: <SiExpress size={32} />,
+          color: "#f1f1f1ff",
+          name: "Express.js",
+        },
+      ],
       images: [
         "/projects/vinyl_store/folders.PNG",
         "/projects/vinyl_store/controller.PNG",
@@ -141,12 +169,16 @@ tech: [
       ],
       link: "https://github.com/SebastianR0589/Vinyl_Store",
     },
-     {
+    {
       name: "SQL Sample Collection",
       framework: "SQL",
-               tech: [
-      { icon: <SiPostgresql size={32} />, color: "#336791", name: "PostgreSQL" },
-    ],
+      tech: [
+        {
+          icon: <SiPostgresql size={32} />,
+          color: "#336791",
+          name: "PostgreSQL",
+        },
+      ],
       images: [
         "/projects/sql_sample/base.PNG",
         "/projects/sql_sample/data.PNG",
